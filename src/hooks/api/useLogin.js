b14829c57228d0529bc08bdaee0ebc08fbe46989
zachHulse <zhulse@accendero.com>
2021-05-20 10:08:36 -0600

    add mutation for logging in

---
+++ src/hooks/api/useLogin.js
@@ -0,0 +1,26 @@
+import { useMutation } from 'react-query';
+
+import request from './request';
+import * as constants from '../../constants';
+
+const login = async (credentials) => {
+  const { data } = await request(constants.LOGIN_URL, {
+    method: 'post',
+    data: credentials,
+  });
+  return data;
+};
+
+export default function useLogin() {
+  return useMutation(login, {
+    onSuccess: async (data, variables) => {
+      await localStorage.setItem(
+        'session',
+        JSON.stringify({
+          email: variables.email,
+          token: data.token,
+        }),
+      );
+    },
+  });
+}
