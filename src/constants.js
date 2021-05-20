357c8086ade8fa34f9509407627e6a38a3c9335d
zachHulse <zhulse@accendero.com>
2021-05-20 10:37:31 -0600

    add global hook for handling application messages

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
 export const API_URL = 'http://localhost:8000/api/';
 export const LOGIN_URL = 'login/';
 export const AUTO_LOGIN_URL = 'auto_login/';
+
+// messages
+export const MESSAGE_TYPE_ERROR = 'error';
+export const MESSAGE_TYPE_SUCCESS = 'success';
+export const MESSAGE_TYPE_WARNING = 'warning';
+export const MESSAGE_TYPE_INFO = 'info';
---
+++ src/hooks/useMessages.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+import * as constants from '../constants';
+
+let messages = {};
+let listeners = [];
+
+const setMessages = (newMessages) => {
+  messages = newMessages;
+  listeners.forEach((listener) => listener(messages));
+};
+
+export const addMessage = (text, type = constants.MESSAGE_TYPE_INFO) => {
+  setMessages({ ...messages, text: type });
+};
+
+const removeMessage = (text) => {
+  const newMessages = { ...messages };
+  delete newMessages[text];
+  setMessages(newMessages);
+};
+
+export default function useMessages() {
+  const newListener = useState()[1];
+  useEffect(() => {
+    // Called just after component mount
+    listeners.push(newListener);
+    return () => {
+      // Called just before the component unmount
+      listeners = listeners.filter((listener) => listener !== newListener);
+    };
+  }, [newListener]);
+  return { messages, addMessage, removeMessage };
+}
