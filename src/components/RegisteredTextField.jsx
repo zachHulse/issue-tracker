79585b1338023d538597a0a77172c7fa7da54b16
zachHulse <zhulse@accendero.com>
2021-05-24 13:25:30 -0600

    add string title helper function

--- src/components/RegisteredTextField.jsx
+++ src/components/RegisteredTextField.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
 import * as propTypes from 'prop-types';
+import { title } from '../string';
 
 const RegisteredTextField = ({ fieldName, register }) => {
   const inputProps = register(fieldName);
   const { name, onBlur, onChange, ref } = inputProps;
-  const label = name
-    .split('_')
-    .map((w) => w[0].toUpperCase() + w.substr(1).toLowerCase())
-    .join('');
+  const label = title(name);
   return (
     <TextField fullWidth variant="outlined" {...{ name, onBlur, onChange, inputRef: ref, label }} />
   );
--- src/string.js
+++ src/string.js
@@ -1,3 +1,9 @@
 export default function replace(str, ...rest) {
   return rest.reduce((prev, curr) => prev.replace('{}', curr), '');
 }
+
+export const title = (str) =>
+  str
+    .split('_')
+    .map((w) => w[0].toUpperCase() + w.substr(1).toLowerCase())
+    .join('');
