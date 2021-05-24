6112e31787a832f5d515ec5e818519bc80499fc4
zachHulse <zhulse@accendero.com>
2021-05-24 13:25:52 -0600

    add DataTable.jsx reusable table component

---
+++ src/components/DataTable.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  withStyles,
+} from '@material-ui/core';
+import * as propTypes from 'prop-types';
+import { title } from '../string';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const DataTable = ({ useData, displayKeys }) => {
+  const { data = [], isLoading } = useData();
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {displayKeys.map((key) => (
+              <StyledTableCell>{title(key)}</StyledTableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <StyledTableRow key={item.id}>
+              <StyledTableCell component="th" scope="row">
+                {item.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{item.description}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {isLoading && <Typography variant="overline">Loading . . .</Typography>}
+    </TableContainer>
+  );
+};
+
+DataTable.propTypes = {
+  useData: propTypes.func.isRequired,
+  displayKeys: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default DataTable;
