c6d323d0256c233897381d09afeef5662db312d5
zachHulse <zhulse@accendero.com>
2021-05-24 20:10:58 -0600

    fix home page display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, Container, Typography } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
 const Home = () => {
   return (
     <Container>
-      <Card variant="outlined">
-        <CardHeader>
-          <Typography variant="h2" color="textSecondary">
-            Issue Tracker
-          </Typography>
-        </CardHeader>
-        <CardContent>
-          <Typography variant="body1" color="textSecondary">
-            simple issue tracking
-          </Typography>
-        </CardContent>
-      </Card>
+      <Typography variant="h2" color="textSecondary" align="center">
+        Issue Tracker
+      </Typography>
+      <Typography variant="body1" color="textSecondary" align="center">
+        simple issue tracking
+      </Typography>
     </Container>
   );
 };
