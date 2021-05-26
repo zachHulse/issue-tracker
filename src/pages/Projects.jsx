cb143aa086970df60743ed85a733601d5d123352
zachHulse <zhulse@accendero.com>
2021-05-26 08:13:07 -0600

    use generic list view in Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,33 +1,10 @@
 import React from 'react';
-
-import { Container, Typography, makeStyles, Grid, Button } from '@material-ui/core';
-import { Link } from 'react-location';
-import { AddCircle } from '@material-ui/icons';
 import useProjects from '../hooks/api/useProjects';
-import DataTable from '../components/DataTable';
-
-const useStyles = makeStyles(() => ({
-  link: {
-    textDecoration: 'none',
-  },
-}));
+import ListView from './generic/ListView';
 
 const Projects = () => {
-  const classes = useStyles();
   return (
-    <Container>
-      <Typography variant="h2">Projects</Typography>
-      <Grid container justify="flex-end">
-        <Grid item>
-          <Link to="create" className={classes.link}>
-            <Button variant="contained" color="default" startIcon={<AddCircle />}>
-              Create Project
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
-      <DataTable displayKeys={['name', 'description']} useData={useProjects} />
-    </Container>
+    <ListView modelName="project" displayKeys={['name', 'description']} useData={useProjects} />
   );
 };
 
