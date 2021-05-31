687380c4602a4ec4d6d2e7a9be70982e14595b08
zachHulse <zhulse@accendero.com>
2021-05-31 11:13:56 -0600

    pass type into RegisteredTextField.jsx

--- src/components/fields/RegisteredTextField.jsx
+++ src/components/fields/RegisteredTextField.jsx
@@ -3,18 +3,29 @@ import { TextField } from '@material-ui/core';
 import * as propTypes from 'prop-types';
 import { title } from '../../string';
 
-const RegisteredTextField = ({ fieldName, register }) => {
+const RegisteredTextField = ({ fieldName, register, type }) => {
   const inputProps = register(fieldName);
   const { name, onBlur, onChange, ref } = inputProps;
   const label = title(name);
   return (
-    <TextField fullWidth variant="outlined" {...{ name, onBlur, onChange, inputRef: ref, label }} />
+    <TextField
+      fullWidth
+      variant="outlined"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      {...{ name, onBlur, onChange, inputRef: ref, label, type }}
+    />
   );
 };
 
 RegisteredTextField.propTypes = {
   fieldName: propTypes.string.isRequired,
   register: propTypes.func.isRequired,
+  type: propTypes.string,
+};
+RegisteredTextField.defaultProps = {
+  type: 'text',
 };
 
 export default RegisteredTextField;
