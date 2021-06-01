cacaf325040edccb4c9508a315fe626b8918617e
zachHulse <zhulse@accendero.com>
2021-06-01 00:24:47 -0600

    add menu to ProjectUpdate.jsx

--- src/pages/ProjectUpdate.jsx
+++ src/pages/ProjectUpdate.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Grid, IconButton, makeStyles, Menu, MenuItem, Typography } from '@material-ui/core';
 import * as propTypes from 'prop-types';
+import { Menu as MenuIcon } from '@material-ui/icons';
+import { useNavigate } from 'react-location';
 import { useProject, useProjectSave } from '../hooks/api/useProjects';
 import SaveView from './generic/SaveView';
 import Project from '../types/project';
 
-const HeaderElement = ({ item }) => <Typography variant="h2">{item?.name}</Typography>;
+const useStyles = makeStyles(() => ({
+  btn: {
+    '& svg': {
+      fontSize: '2em',
+    },
+  },
+}));
+
+const HeaderElement = ({ item }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Grid container justify="space-between">
+      <Typography variant="h2">{item?.name}</Typography>
+      <IconButton
+        aria-controls="menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        className={classes.btn}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => navigate('../sprints')}>Sprints</MenuItem>
+        <MenuItem onClick={() => navigate('../issues')}>Issues</MenuItem>
+      </Menu>
+    </Grid>
+  );
+};
 
 const ProjectUpdate = () => {
   const fields = {
