a5f80c0d6cb6e5a3158e9e84edeb51308539a398
zachHulse <zhulse@accendero.com>
2021-06-01 23:43:49 -0600

    breadcrumbs styling

--- src/components/Crumbs.jsx
+++ src/components/Crumbs.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link, useLocation } from 'react-location';
 import { NavigateNext } from '@material-ui/icons';
-import { Breadcrumbs } from '@material-ui/core';
+import { Breadcrumbs, Typography } from '@material-ui/core';
+import { title } from '../string';
 
 const Crumbs = () => {
   const location = useLocation();
@@ -10,7 +11,11 @@ const Crumbs = () => {
   return (
     <Breadcrumbs separator={<NavigateNext />}>
       {partials.map((part, index) => (
-        <Link to={partials.slice(0, index + 1).join('/')}>{part}</Link>
+        <Link to={partials.slice(0, index + 1).join('/')}>
+          <Typography variant="h5" color="textSecondary">
+            {title(part)}
+          </Typography>
+        </Link>
       ))}
     </Breadcrumbs>
   );
