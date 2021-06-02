c8dad13e8887c643e7d6b6ae1bbb87406427796d
zachHulse <zhulse@accendero.com>
2021-06-02 00:34:32 -0600

    store isAdmin in session

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -3,13 +3,15 @@ import { useEffect, useState } from 'react';
 let session = {
   token: localStorage.getItem('token'),
   email: localStorage.getItem('email'),
+  isAdmin: localStorage.getItem('isAdmin'),
 };
 let listeners = [];
 
-export const setSession = (token, email) => {
-  session = { token, email };
+export const setSession = (token, email, isAdmin) => {
+  session = { token, email, isAdmin };
   localStorage.setItem('token', token);
   localStorage.setItem('email', email);
+  localStorage.setItem('isAdmin', isAdmin);
   listeners.forEach((listener) => listener(session));
 };
 
@@ -17,6 +19,7 @@ export const clearSession = () => {
   session = {};
   localStorage.removeItem('token');
   localStorage.removeItem('email');
+  localStorage.removeItem('isAdmin');
 };
 
 export default function useSession() {
